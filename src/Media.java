cb1b33f0d56c20d12a00ae138b09568c9e351cc5
Guilherme de Oliveira Santos <gui09106@gmail.com>
2026-09-08 21:43:35 -0300

    feat: adiciona sistema média e validação de notas

--- src/Media.java
+++ src/Media.java
@@ -8,49 +8,95 @@ public class Media
 
         String nome;
         float nota1, nota2, nota3, media;
-
-        System.out.println("Qual o seu nome?");
-        nome = resp.next();
+        char continuar;
 
         do {
 
-            System.out.print("Primeira nota foi: ");
-            nota1 = resp.nextFloat();
 
-        }while(nota1 < 0 || nota1 > 10);
+            System.out.println("Qual o seu nome?");
+            nome = resp.next();
 
-        do {
+            do {
 
-            System.out.print("Segunda nota foi: ");
-            nota2 = resp.nextFloat();
+                System.out.print("Primeira nota foi: ");
 
-        }while(nota2 < 0 || nota2 > 10);
+                if(resp.hasNextFloat())
 
-        do {
+                   nota1 = resp.nextFloat();
 
-            System.out.print("Terceira nota foi: ");
-            nota3 = resp.nextFloat();
 
-        }while(nota3 < 0 || nota3 > 10);
+                else
+                {
+                    System.out.println("Digite sua NOTA!");
+                    resp.next();
+                    nota1 = -1;
+                }
 
-        media = (nota1 + nota2 + nota3) / 3;
+            } while (nota1 < 0 || nota1 > 10);
 
-        if (media >= 7)
-        {
 
-            System.out.println("APROVADO");
-            System.out.printf("Sua nota foi: %.1f", media);
+            do {
 
-        } else if (media >= 5)
-        {
-            System.out.println("RECUPERAÇÃO");
-            System.out.printf("Sua nota foi: %.1f", media);
-        }
-        else
-        {
-            System.out.println("REPROVADO");
-            System.out.printf("Sua nota foi: %.1f", media);
-        }
+                System.out.print("Segunda nota foi: ");
+                if(resp.hasNextFloat())
+
+                   nota2 = resp.nextFloat();
+
+                else
+                {
+                    System.out.println("Digite sua NOTA!");
+                    resp.next();
+                    nota2 = -1;
+                }
+
+            } while (nota2 < 0 || nota2 > 10);
+
+
+
+            do {
+
+                System.out.print("Terceira nota foi: ");
+                if(resp.hasNextFloat())
+
+                   nota3 = resp.nextFloat();
+
+                else
+                {
+                    System.out.println("Digite sua NOTA!");
+                    resp.next();
+                    nota3 = -1;
+                }
+
+            } while (nota3 < 0 || nota3 > 10);
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            if (media >= 7) {
+
+                System.out.printf(nome + ", sua nota foi %.1f", media);
+                System.out.println();
+                System.out.print("Situação: APROVADO");
+
+            } else if (media >= 5) {
+                System.out.printf(nome + ", sua nota foi %.1f", media);
+                System.out.println();
+                System.out.print("Situação: RECUPERÇÃO");
+            } else {
+                System.out.printf(nome + ", sua nota foi %.1f", media);
+                System.out.println();
+                System.out.print("Situação: REPROVADO");
+            }
+
+            System.out.println();
+
+            do {
+
+                System.out.println("Deseja Cadastrar outro Aluno?(S/N)");
+                continuar = resp.next().charAt(0);
+
+            }while(continuar != 'N' && continuar != 'S' && continuar != 'n' && continuar != 's' );
+
+        }while(continuar == 'S' || continuar == 's');
 
 
     }
